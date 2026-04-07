8cd677d71f7771cbc1668a34075d87b5a6f94d61
RAKOTONDRATREMA Sarobidinirina Fanantenana <sarobidinirinarakoto@gmail.com>
2026-04-07 13:48:12 +0300

    Update exo6_3_b.c

--- Chapitre_6/exo6_3_b.c
+++ Chapitre_6/exo6_3_b.c
@@ -5,7 +5,15 @@
 
 int main()
 {
+    int ret,i;
     int *tab=malloc(sizeof(tab)*sizeof(int));
     int fd = open("tableau.txt",O_RDONLY,0764);
-
+    i=0;
+    do
+    {
+        ret=read(fd,&tab[i],sizeof(int)) ;
+        printf("tab[%d]=%d\n",i,tab[i]);
+        i++;
+    } while (ret > 0);    
+    return(0);
 }
